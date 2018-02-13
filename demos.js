6fd247e798f68668f80051d26e21d334f10ca9c8
Dominic Tarr <dominic.tarr@gmail.com>
2018-02-13 15:28:55 +1300

    demos

--- demos.js
+++ demos.js
@@ -1,9 +1,9 @@
 var h = require('hyperscript')
-var random = require('network-simulator/random')
-var animate = require('network-simulator/animate')
+var random = require('network-animator/random')
+var animate = require('network-animator/animate')
 var G = require('graphreduce')
 var h = require('hyperscript')
-var random = require('network-simulator/random')
+var random = require('network-animator/random')
 
 var W = 600, H = 600
 
@@ -11,7 +11,26 @@ function canvas () {
   return h('canvas', {width: W, height: H})
 }
 
-exports.centralized = function () {
+function starter (fn) {
+  var stopped = false
+  function stop () {
+    return stopped
+  }
+  return function () {
+    var d = h('div')
+    d.addEventListener('focus', function () {
+      stopped = false
+      d.innerHTML = ''
+      d.appendChild(fn(stop))
+    })
+    d.addEventListener('blur', function () {
+      stopped = true
+    })
+    return d
+  }
+}
+
+exports.centralized = starter(function (stop) {
   var c = canvas() //
   var g = {}
   var N = 200
@@ -32,12 +51,12 @@ exports.centralized = function () {
         delete g[packets[i].to][packets[i].from]
       }
     label.textContent = e + '/' + packets.length
-    animate(g, loc, c.getContext('2d'), next)
+    if(!stop()) animate(g, loc, c.getContext('2d'), next)
   })
   return h('div', c, label)
-}
+})
 
-exports.random = function () {
+exports.random = starter(function (stop) {
 
   var c = canvas() //h('canvas', {width: W, height: H})
   var g = random.graph(200, 3)
@@ -49,12 +68,12 @@ exports.random = function () {
     for(var i in packets)
       if(packets[i].extra) e ++
     label.textContent = e + '/' + packets.length
-    animate(g, loc, c.getContext('2d'), next)
+    if(!stop()) animate(g, loc, c.getContext('2d'), next)
   })
   return h('div', c, label)
-}
+})
 
-exports.spanning = function () {
+exports.spanning = starter(function () {
   var c = canvas() //h('canvas', {width: W, height: H})
   var g = random.graph(200, 3)
   var loc = random.locations(200, c.width, c.height)
@@ -69,47 +88,49 @@ exports.spanning = function () {
         delete g[packets[i].to][packets[i].from]
       }
     label.textContent = e + '/' + packets.length
-    animate(g, loc, c.getContext('2d'), next)
+    if(!stop()) animate(g, loc, c.getContext('2d'), next)
   })
   return h('div', c, label)
 
-}
-exports.fragile = function () {
+})
+exports.fragile = starter(function () {
   var N = 200
   var c = canvas()
-  var g = random.graph(N, 3)
   var loc = random.locations(N, c.width, c.height)
   var label = h('label')
 
-  animate(g, loc, c.getContext('2d'), function (_, packets) {
-    var e = 0
-    for(var i in packets)
-      if(packets[i].extra) {
-        e ++
-        delete g[packets[i].from][packets[i].to]
-        delete g[packets[i].to][packets[i].from]
+  //;(function next () {
+    var g = random.graph(N, 2)
+
+    animate(g, loc, c.getContext('2d'), function next (_, packets) {
+      var e = 0
+      for(var i in packets)
+        if(packets[i].extra) {
+          e ++
+          delete g[packets[i].from][packets[i].to]
+          delete g[packets[i].to][packets[i].from]
+        }
+
+      //randomly delete some nodes
+      var d
+      for(var i = 0; i < 1; i++) {
+        var d = ~~(Math.random()*N)
+        delete g[d]
+        for(var i in g[d])
+          delete g[i][d]
       }
 
-    //randomly delete one node
-    for(var i = 0; i < 3; i++) {
-      var d = ~~(Math.random()*N)
-      delete g[d]
-      for(var i in g[d])
-        delete g[i][d]
-    }
-
-    label.textContent = e + '/' + packets.length
-    animate(g, loc, c.getContext('2d'), function next (_, packets) {
-      e = 0
-      for(var i in packets)
-        if(packets[i].extra)
-          e ++
       label.textContent = e + '/' + packets.length
-      animate(g, loc, c.getContext('2d'), next)
+      if(!stop()) animate(g, loc, c.getContext('2d'), next)
     })
-  })
+
+  //})()
   return h('div', c, label)
 
-}
+})
+
+
+
+
 
 
